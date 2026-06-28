f75ab61771c94a9fb1c526592d5d5b1788e1a8be
jordanmoore0120-cmd <jordanmoore0120@gmail.com>
2026-06-28 09:09:29 +0000

    content-freshness: dateModified → 2026-06-28 (service pages)

--- src/app/[city]/[service]/page.tsx
+++ src/app/[city]/[service]/page.tsx
@@ -243,7 +243,7 @@ export default async function ServiceHubPage({ params }: ServiceHubProps) {
     aggregateRating: { "@type": "AggregateRating", ratingValue: "4.84", reviewCount: "2000", bestRating: "5" },
     areaServed: { "@type": "City", name: city.name },
     serviceType: service.name,
-    dateModified: "2026-06-21",
+    dateModified: "2026-06-28",
     hasOfferCatalog: {
       "@type": "OfferCatalog",
       name: `${service.name} Services`,
